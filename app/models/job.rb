254c3ef2d50571a07ea560f83ff8f2e112797d4c
gionaufal <g10v45+github@gmail.com>
2016-09-09 21:56:01 -0300

    expired jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,8 @@ class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
   validates :title, :location, :description, :category_id,  presence: true
+
+  def expired?
+    Time.now.to_date - created_at.to_date > 88
+  end
 end
