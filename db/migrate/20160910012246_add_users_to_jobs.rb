928dd72ea9d73c66d1f38c61ff5935e4bbe954e3
gionaufal <g10v45+github@gmail.com>
2016-09-20 08:40:59 -0300

    remove migration files

--- db/migrate/20160910012246_add_users_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersToJobs < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :jobs, :users, foreign_key: true
-  end
-end
--- db/migrate/20160911203005_remove_users_from_jobs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUsersFromJobs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :jobs, :users, :string
-  end
-end
